d666f311892fdd4e9e04e19a23a88387def8fcc1
Ben Derbecker <derbecb@wwu.edu>
2025-04-12 22:35:01 -0700

    fixing bugs

--- VideoSplicer.py
+++ VideoSplicer.py
@@ -1,5 +1,5 @@
 import tkinter as tk
-from tkinter import filedialog, Variable
+from tkinter import filedialog, StringVar, Variable
 import ffmpeg
 
 root = tk.Tk()
@@ -10,35 +10,35 @@ root.geometry("500x400")
 
 time_slot1 = Variable
 time_slot2 = Variable
-grab_video_path = ""
-render_video_path = ""
+grab_video_path = StringVar()
+render_video_path = StringVar()
 
 def grab_video():
-    grab_video_path = filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4")])
+    grab_video_path.set(filedialog.askopenfilename(filetypes=[("Video Files", "*.mp4")]))
 
 
-def render_video_path():
-    render_video_path = filedialog.askdirectory()
+def set_render_video_path():
+    render_video_path.set(filedialog.askdirectory())
 
 tk.Button(root, text="Grab Video", command=grab_video).grid(row=0, column=0, padx=10, pady=10)
-tk.Button(root, text="Set Render Path", command=render_video_path).grid(row=0, column=1, padx=10, pady=10)
-tk.Label(root, text = "Enter paths in the format of  Hours:Minutes:Seconds").grid(row=1, column=0, padx=10, pady=10)
+tk.Button(root, text="Set Render Path", command=set_render_video_path).grid(row=0, column=1, padx=10, pady=10)
+tk.Label(root, text = "Enter paths in the format of  Hours:Minutes:Seconds").grid(row=3, column=0, padx=10, pady=10)
 
-tk.Label(root, text="Enter first video timestamp").grid(row=2, column=0)
-tk.Label(root, text="Enter second video timestamp").grid(row=3, column=0)
+tk.Label(root, text="Enter first video timestamp").grid(row=4, column=0)
+tk.Label(root, text="Enter second video timestamp").grid(row=5, column=0)
 time_slot1 = tk.Entry(root)
 time_slot2 = tk.Entry(root)
 
-time_slot1.grid(row=2, column=1)
-time_slot2.grid(row=3, column=1)
+time_slot1.grid(row=4, column=1)
+time_slot2.grid(row=5, column=1)
+
+
+tk.Label(root, textvariable=grab_video_path).grid(row=1, column=0, padx=10, pady=10)
+tk.Label(root, textvariable=render_video_path).grid(row=1, column=0, padx=10, pady=10)
 
 
 menu = tk.Menu(root)
 root.config(menu=menu)
 menu.add_cascade(label='Exit', command=root.destroy)
-
-root.mainloop()
-
-
 
 root.mainloop()
